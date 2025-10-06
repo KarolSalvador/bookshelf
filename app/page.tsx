33abacdae96be00634bcf76164c22138c0ae71c5
KarolSalvador <karolsalvador91@gmail.com>
2025-10-06 19:07:26 -0300

    feat: Adiciona imagem a tela inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center p-4 text-center">
       <h1 className="text-4xl font-bold mb-4">Bem-vindo(a) ao BookShelf!</h1>
-      <p className="text-lg text-muted-foreground">
+      <p className="text-lg text-muted-foreground mb-4">
         Sua biblioteca pessoal está pronta para ser construída.
       </p>
+
+      <Image
+        src="/images/Gemini_Generated_bookshelf.png"
+        alt="Ilustração de uma estante de livros para acompanhar leitura"
+        width={700}
+        height={500}
+        priority
+        className="rounded-lg shadow-lg"
+      />
     </div>
   );
 }
