09076f9a8f5579f058f409ef78658f241a6c2780
KarolSalvador <karolsalvador91@gmail.com>
2025-10-07 16:09:46 -0300

    fix(vercel): Corrigir deploy no Vercel com SQLite

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,19 @@
 import { PrismaClient } from "./generated/prisma";
+import path from "path";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+let databaseUrl = process.env.DATABASE_URL;
+
+if (databaseUrl && databaseUrl.startsWith("file:")) {
+  const relativePath = databaseUrl.replace("file:", "");
+
+  const absolutePath = path.resolve(relativePath);
+
+  databaseUrl = `file:${absolutePath}`;
+}
 
 //Criação d ainstância do Prisma Client
 export const prisma =
@@ -12,6 +23,7 @@ export const prisma =
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
+    ...(databaseUrl && { datasources: { db: { url: databaseUrl } } }),
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
